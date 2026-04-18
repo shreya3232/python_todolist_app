3c3c2b6ba3231802677df54ff5bd3cd871724563
Shreyas Gowda <shreyasgowda2345@gmail.com>
2026-04-18 23:37:21 +0530

    my python project

---
+++ todo_project/todo/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all().order_by('-created')
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'index.html', context)
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'update.html', context)
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    context = {'task': task}
+    return render(request, 'delete.html', context)
